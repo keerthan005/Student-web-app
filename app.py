0647435910823167b1a17904633b80a95296242d
keerthan005 <107531341+keerthan005@users.noreply.github.com>
2025-07-25 15:19:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,4 @@
-from flask import Flask, request, jsonify, render_template
-import pickle
-
-app = Flask(__name__)
-
-# Load model once at startup
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-@app.route('/')
-def home():
-    return "Student Performance Prediction Flask API is running!"
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        data = request.get_json(force=True)
-
-        input_features = [
-            data['gender'],
-            data['parental_level_of_education'],
-            data['test_preparation_course'],
-            data['reading_score'],
-            data['writing_score']
-        ]
-
-        prediction = model.predict([input_features])
-        return jsonify({'Predicted Class': int(prediction[0])})
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+from flask import render_template, request
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
@@ -44,6 +14,3 @@ def form():
         prediction = model.predict([input_features])[0]
 
     return render_template('form.html', prediction=prediction)
-
-if __name__ == '__main__':
-    app.run(debug=True)
